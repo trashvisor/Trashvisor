5ad4117306b48e00bad0a0688c0b0478d52dac39
trashvisor <64044176+trashvisor@users.noreply.github.com>
2020-08-07 20:48:58 +0200

    Removed the correct file...

--- Trashvisor/VmxHostExitHandlers.c
+++ Trashvisor/VmxHostExitHandlers.c
@@ -3,31 +3,42 @@
 
 _Use_decl_annotations_
 VOID
-HandleCpuid(
+ExitHandleCpuid (
     PVMM_EXIT_CONTEXT pExitContext,
-    PGUEST_REGISTER_CONTEXT pGPContext
+    PVMM_GUEST_CONTEXT pGPContext
 )
 {
-    INT32 Cpuid[4];
-    CR4 Cr4;
+    pExitContext->IncrementRip = TRUE;
+
+    UINT64 StoredRax = pGPContext->Rax;
+
+    int Cpuid[4];
+
+    __cpuid(Cpuid, pGPContext->Rax);
+
+    pGPContext->Rax = Cpuid[0];
+    pGPContext->Rbx = Cpuid[1];
+    pGPContext->Rcx = Cpuid[2];
+    pGPContext->Rdx = Cpuid[3];
 
     if (pCpuidLoggingInfo == NULL)
         return;
 
     if (pExitContext->GuestCr3.Flags == pCpuidLoggingInfo->UserCr3.Flags)
     {
-        // Adjust CR3 to the guest's CR3.
-        // Required for usermode memory inspection
-
         __writecr3(pCpuidLoggingInfo->KernelCr3.Flags);
+
+        KdPrintError(
+            "Cpuid called for process with RAX: 0x%llx\n",
+            StoredRax
+        );
 
         PCHAR pPeb = (PCHAR)pCpuidLoggingInfo->pPeb;
 
         // Disable SMAP
-        // Required to iterate the usermode PEB
+        CR4 Cr4;
         Cr4.Flags = __readcr4();
         Cr4.SmapEnable = 0;
-
         __writecr4(Cr4.Flags);
 
         PPEB_LDR_DATA pLdrData = *(PUINT64)(pPeb + 0x18);
@@ -50,7 +61,7 @@ HandleCpuid(
 
             if (pExitContext->GuestRip >= BeginAddress && pExitContext->GuestRip <= EndAddress)
             {
-                DataLine.CpuidRax = pGPContext->Rax;
+                DataLine.CpuidRax = StoredRax;
                 DataLine.CpuidModuleRva = pExitContext->GuestRip - BeginAddress;
 
                 RtlCopyMemory(
@@ -59,14 +70,14 @@ HandleCpuid(
                     pCurrentModule->BaseDllName.Length
                 );
 
-                KeAcquireGuardedMutex(&CallbacksMutex);
+                KdPrintError("Found cpuid call with RVA: 0x%x in: %wZ\n",
+                    pExitContext->GuestRip - BeginAddress,
+                    pCurrentModule->BaseDllName);
 
-                if (pCpuidLoggingInfo->CurrentDataLine < pCpuidLoggingInfo->DataLineLimit)
-                    ;//pCpuidLoggingInfo->LoggingData[pCpuidLoggingInfo->CurrentDataLine++] = DataLine;
+                if (pCpuidLoggingInfo->CurrentDataLine < pCpuidLoggingInfo->MaxLogCount)
+                    pCpuidLoggingInfo->LoggingData[pCpuidLoggingInfo->CurrentDataLine++] = DataLine;
                 else
-                    KdPrintError("Readjust IOCTL data limit.\n");
-
-                KeReleaseGuardedMutex(&CallbacksMutex);
+                    KdPrintError("Adjust the max logging count!.\n");
 
                 break;
             }
@@ -75,29 +86,17 @@ HandleCpuid(
 
         } while (CurrLink->Flink != FirstLink);
 
-        // Reset CR4 to its original value
         Cr4.SmapEnable = 1;
         __writecr4(Cr4.Flags);
 
-        // Reset CR3 to its original value
-        __writecr3(pExitContext->GuestCr3.Flags);
+        // Could rewrite cr3 but no point
     }
-//CpuidHandler:
-/*
-__cpuid(Cpuid, pGPContext->Rax);
-
-pGPContext->Rax = Cpuid[0];
-pGPContext->Rbx = Cpuid[1];
-pGPContext->Rcx = Cpuid[2];
-pGPContext->Rdx = Cpuid[3];
-*/
 }
 
-_Use_decl_annotations_
 VOID
-HandleRdMsr(
-    PVMM_EXIT_CONTEXT pExitContext,
-    PGUEST_REGISTER_CONTEXT pGPContext
+ExitHandleRdMsr (
+    _In_ PVMM_EXIT_CONTEXT pExitContext,
+    _In_ PVMM_GUEST_CONTEXT pGPContext
 )
 {
     ULONG64 Value = __readmsr(pGPContext->Rcx);
@@ -105,11 +104,10 @@ HandleRdMsr(
     pGPContext->Rdx = (Value >> 32) & 0xFFFFFFFF;
 }
 
-_Use_decl_annotations_
 VOID
-HandleWrMsr(
-    PVMM_EXIT_CONTEXT pExitContext,
-    PGUEST_REGISTER_CONTEXT pGPContext
+ExitHandleWrMsr (
+    _In_ PVMM_EXIT_CONTEXT pExitContext,
+    _In_ PVMM_GUEST_CONTEXT pGPContext
 )
 {
     ULONG64 Value = pGPContext->Rdx;
